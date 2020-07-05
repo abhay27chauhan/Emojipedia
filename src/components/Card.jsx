c1d46852aa685a8dfe5941aad02b28e5afc349ea
ABHAY CHAUHAN <42849043+abhay27chauhan@users.noreply.github.com>
2020-07-05 18:28:12 +0530

    Initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Card(props) {
+  return (
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label="Tense Biceps">
+          {props.emoji}
+        </span>
+        <span>{props.name}</span>
+      </dt>
+      <dd>{props.means}</dd>
+    </div>
+  );
+}
+
+export default Card;
